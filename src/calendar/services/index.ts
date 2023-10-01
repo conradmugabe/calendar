66a18d3274d3890729f7057bbf2a3a4a94dcef7e
conrad mugabe <conradmugabe@gmail.com>
2023-10-01 21:42:20 +0300

    feat: switched from fetch to axios

--- src/calendar/services/index.ts
+++ src/calendar/services/index.ts
@@ -1,18 +1,22 @@
+import axios from "axios";
+
 export type CalendarSettings = {
   view: string;
 };
 
 export class CalendarSettingsService {
   create = async (settings: CalendarSettings): Promise<CalendarSettings> => {
-    const response = await fetch(process.env.CALENDAR_API_URL || "", {
-      method: "POST",
-      body: JSON.stringify(settings),
-    });
-    return response.json();
+    const response = await axios.post<CalendarSettings>(
+      process.env.CALENDAR_API_URL || "",
+      settings,
+    );
+    return response.data;
   };
 
   get = async (): Promise<CalendarSettings> => {
-    const response = await fetch(process.env.CALENDAR_API_URL || "");
-    return response.json();
+    const response = await axios.get<CalendarSettings>(
+      process.env.CALENDAR_API_URL || "",
+    );
+    return response.data;
   };
 }
