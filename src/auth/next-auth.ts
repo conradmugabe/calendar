50714e9ccb0e89638103ca13213ed121203dc947
conrad mugabe <conradmugabe@gmail.com>
2023-10-12 23:02:55 +0300

    feat: added google auth

--- src/auth/next-auth.ts
+++ src/auth/next-auth.ts
@@ -1,3 +1,12 @@
 import { AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-export const nextAuthOptions: AuthOptions = { providers: [] };
+export const nextAuthOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: { strategy: "jwt" },
+};
