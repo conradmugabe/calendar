2ce06bb215ddf97169ac168e418e242d7ec4063d
conrad mugabe <conradmugabe@gmail.com>
2023-11-10 01:30:43 +0300

    feat: added loading skeleton for user profile dropdown

--- src/components/compound/profile-dropdown.tsx
+++ src/components/compound/profile-dropdown.tsx
@@ -2,10 +2,12 @@
 
 import { UserButton, useUser } from "@clerk/nextjs";
 
+import { Skeleton } from "@/components/common/skeleton";
+
 export function ProfileDropdown() {
   const { isLoaded, user } = useUser();
 
   if (isLoaded && user) return <UserButton afterSignOutUrl="/" />;
 
-  return <div>Loading...</div>;
+  return <Skeleton className="h-8 w-8 rounded-full bg-gray-400" />;
 }
