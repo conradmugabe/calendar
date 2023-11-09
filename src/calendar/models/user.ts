01de9aa03bd7eb699a0fd5d6db23af3d56488180
conrad mugabe <conradmugabe@gmail.com>
2023-11-10 01:32:13 +0300

    feat: added loading skeleton for user profile dropdown

--- src/calendar/models/user.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  image: String,
-});
-
-export const User = mongoose.model("users", userSchema);
